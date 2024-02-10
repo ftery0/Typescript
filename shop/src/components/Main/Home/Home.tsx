8391aab133c1351dcfd04e62c89a04b41e7d6766
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-10 22:49:29 +0900

    test Eventview

--- shop/src/components/Main/Home/Home.tsx
+++ shop/src/components/Main/Home/Home.tsx
@@ -1,11 +1,19 @@
 import * as S from "./style"
 import Header from "src/constants/Head/Head"
-
+import testImg from "src/Assets/img/testImg.jpg"
 const Home =()=>{
 return(
     <S.Main>
         <Header/>
-        
+        <S.Event>
+            <S.Advertisement>
+                <S.AdvertisementImg src={testImg}></S.AdvertisementImg>
+                <S.AdvertisementImg src={testImg}></S.AdvertisementImg>
+                <S.AdvertisementImg src={testImg}></S.AdvertisementImg>
+                <S.AdvertisementImg src={testImg}></S.AdvertisementImg>
+                <S.AdvertisementImg src={testImg}></S.AdvertisementImg>
+            </S.Advertisement>
+        </S.Event>
     </S.Main>
 )
 }
