adb67b758adb64251e3e0efeafbafb2ab08b5ac4
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-12 02:11:04 +0900

    feat :: 베스트 셀러

--- shop/src/components/Main/Home/Home.tsx
+++ shop/src/components/Main/Home/Home.tsx
@@ -1,6 +1,7 @@
 import * as S from "./style"
 import Header from "src/constants/Head/Head"
-import testImg from "src/Assets/img/testImg.jpg"
+import testImg from "src/Assets/img/fashonEvent.webp"
+import product from "src/Assets/img/product.png"
 const Home =()=>{
 return(
     <S.Main>
@@ -14,6 +15,29 @@ return(
                 <S.AdvertisementImg src={testImg}></S.AdvertisementImg>
                 <S.AdvertisementImg src={testImg}></S.AdvertisementImg>
             </S.Advertisement>
+            <S.BestSellerText><p>베스트셀러</p> </S.BestSellerText>
+            <S.BestSeller>
+                <S.BestSellerDetail>
+                <S.BestSellerProduct>
+                <S.BestSellerImg src={product}></S.BestSellerImg>
+                </S.BestSellerProduct>
+                <S.BestSellerProduct>
+                <S.BestSellerImg src={product}></S.BestSellerImg>
+                </S.BestSellerProduct>
+                <S.BestSellerProduct>
+                <S.BestSellerImg src={product}></S.BestSellerImg>
+                </S.BestSellerProduct>
+                <S.BestSellerProduct>
+                <S.BestSellerImg src={product}></S.BestSellerImg>
+                </S.BestSellerProduct>
+                <S.BestSellerProduct>
+                <S.BestSellerImg src={product}></S.BestSellerImg>
+                </S.BestSellerProduct>
+                <S.BestSellerProduct>
+                <S.BestSellerImg src={product}></S.BestSellerImg>
+                </S.BestSellerProduct>
+                </S.BestSellerDetail>
+            </S.BestSeller>
         </S.Event>
     </S.Main>
 )
--- shop/src/components/Main/Home/style.ts
+++ shop/src/components/Main/Home/style.ts
@@ -1,28 +1,71 @@
+import exp from "constants";
 import styled from "styled-components";
 
-export const Main=styled.div`
-display: flex;
-width: 100vw;
-height: 300vh;
-`
+export const Main = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 300vh;
+`;
 export const Event = styled.div`
-display: flex;
-margin-top: 80px;
-width: 100vw;
-height: calc(100% - 80px);
-`
+  display: flex;
+  flex-direction: column;
+  margin-top: 80px;
+  width: 100vw;
+  height: calc(100% - 80px);
+`;
 export const Advertisement = styled.div`
-margin-top: 50px;
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 49%;
+  overflow: hidden;
+`;
+export const AdvertisementImg = styled.img`
+  margin-top: 20px;
+  display: flex;
+  width: 400px;
+  height: 550px;
+`;
+export const BestSeller = styled.div`
+  margin-top: 20px;
+  display: flex;
+  overflow-y: hidden;
+  width: 100%;
+  height: 20%;
+  flex-direction: column;
+  
+`;
+export const BestSellerText = styled.div`
+  display: flex;
+  margin-top: 20px;
+  width: 100%;
+  height: 50px;
+  p {
+    font-size: 20px;
+    font-weight: 600;
+    margin-left: 50px;
+  }
+`;
+export const BestSellerDetail = styled.div`
+  display: flex;
+  overflow-x: scroll;
+  flex-wrap: nowrap;
+  width: 155vw;
+  height: 100%;
+`;
+export const BestSellerProduct = styled.div`
+margin-left: 30px;
 display: flex;
-justify-content: center;
-gap: 20px;
-flex-wrap: wrap;
-width: 100%;
-height: 50%;
-background-color: aqua;
+width: 250px;
+height: 100%;
+
 `
-export const AdvertisementImg =styled.img`
+export const BestSellerImg = styled.img`
 display: flex;
-width: 400px;
-height: 550px;
+width: 100%;
+height: 60%;
+background-color: brown;
 `
