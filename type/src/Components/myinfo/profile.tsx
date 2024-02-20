995b35674029e6aed244671a09fbb54df82cc8e0
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-21 04:19:37 +0900

    info 추가

--- type/src/Components/myinfo/profile.tsx
+++ type/src/Components/myinfo/profile.tsx
@@ -1,5 +1,8 @@
 import Header from "../head/header"
 import black from "src/Assets/img/black2112.jpeg"
+import sopo from "src/Assets/img/sopo.png"
+import alimo from "src/Assets/img/alimo.png"
+import qvick from "src/Assets/img/qvick.png"
 import "./profile.scss"
 const MyProfile =()=>{
     return(
@@ -7,7 +10,12 @@ const MyProfile =()=>{
          <Header/>
         <div className="Profile">
            <img src={black} alt="" />
-
+            <div className="ranking">
+                <div className="ranking3"><img src={sopo} alt="" /></div>
+                <div className="ranking1"><img src={alimo} alt="" /></div>
+                <div className="ranking2"><img src={qvick} alt="" /></div>
+                
+            </div>
         </div>
         </>
     )
