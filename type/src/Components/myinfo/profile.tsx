dad2edccf44ab838dee2fe3d1ea4ae246780534b
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-21 03:14:46 +0900

    배경 이미지 추가

--- type/src/Components/myinfo/profile.tsx
+++ type/src/Components/myinfo/profile.tsx
@@ -1,11 +1,12 @@
 import Header from "../head/header"
+import black from "src/Assets/img/black2112.jpeg"
 import "./profile.scss"
 const MyProfile =()=>{
     return(
         <>
          <Header/>
         <div className="Profile">
-           
+           <img src={black} alt="" />
 
         </div>
         </>
