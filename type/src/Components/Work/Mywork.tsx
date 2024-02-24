5ec8d577aab5e6b409430148942698b2d3031b2b
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-24 21:00:32 +0900

    link 추가

--- type/src/Components/Work/Mywork.tsx
+++ type/src/Components/Work/Mywork.tsx
@@ -5,15 +5,16 @@ import alimo from "src/Assets/img/alimo.png"
 import qvick from "src/Assets/img/qvick.png"
 import "./work.scss"
 const MyWork =()=>{
+    
     return(
         <>
          <Header/>
         <div className="Profile">
            <img src={black} alt="" />
             <div className="ranking">
-                <div className="ranking3"><img src={sopo} alt="" /></div>
-                <div className="ranking1"><img src={alimo} alt="" /></div>
-                <div className="ranking2"><img src={qvick} alt="" /></div>
+                <div className="ranking3"><img src={sopo} alt="" onClick={()=>window.open('https://sparkling-sting-af7.notion.site/SOPO-600a1747126840339ac79278dd44a657?pvs=4')}/></div>
+                <div className="ranking1"><img src={alimo} alt="" onClick={()=>window.open('https://fast-philosophy-82b.notion.site/ALIMO-6120a6a6cdd64c4eaa936b76dd7e126f?pvs=4')}/></div>
+                <div className="ranking2"><img src={qvick} alt="" onClick={()=>window.open('https://github.com/Team-C0nnect')} /></div>
                 
             </div>
         </div>
